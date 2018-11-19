f49b5e26d80072614963abbe2d5831f9ea3c6cb9
ytraveler <40886593+ytraveler@users.noreply.github.com>
2018-11-19 14:34:56 +0800

    Jl
    第一次上传

---
+++ js/index.js
@@ -0,0 +1,18 @@
+$(window).on('load',function(){
+    $(".loader").delay(500).fadeOut('slow');
+    $("body").css("overflow", "auto");
+    $(".nav-toggle").removeClass("jiji");
+    $(".title").removeClass("jiji");
+    $(".nav-toggle").click(function(){
+        $(".nav-toggle").fadeOut();
+    });
+    $(".overlay-close").click(function(){
+        $(".nav-toggle").fadeIn()
+    });
+    $(".nav li a").click(function(){
+        if($("#overlay").hasClass("open")){
+            $("#overlay").removeClass("open");
+            $(".nav-toggle").fadeIn();
+        }
+    });	
+})
